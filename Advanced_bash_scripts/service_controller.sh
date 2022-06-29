8cb08450fe01eff7e5ddde1c2c2bf280194de505
mozesgit <mozeschioko@gmail.com>
2022-06-29 08:29:29 +0200

    The file has been modified

--- Advanced_bash_scripts/service_controller.sh
+++ Advanced_bash_scripts/service_controller.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
-# THIS PROGRAM IS USED TO CHECK AND THE STATUS OF A SPECIFIED SERVICE
+#:'
+#   AUTHOR :  MOSES CHIOKO
+#   DATE :    29 JUNE 2022
+#   PURPOSE : THIS PROGRAM IS USED TO CHECK AND CHANGE THE STATUS OF A SPECIFIED SERVICE
+#             IF THE SERVICE IS NOT AVAILABLE THE USER HAS THE OPTION TO INSTALL THE PACKAGE(S)
+#             FOR THE SERVICE VIA FIREFOX OR TERMINAL
+#:'
 function service_control()
 {
     service_name=$1
@@ -12,59 +17,196 @@ function service_control()
           echo "**********************************************************"
           echo "The $service_name service is active that is up and running"
           echo "**********************************************************"
-    
-          read -p "Do you want to stop the $service_name [y/n[confirm]]:" agree
+          sleep 3
+          clear
+
+          read -p "Do you want to stop or restart the $service_name [y/n[confirm]]:" agree
 
           if [ $agree == "y" ];then
+              clear 
+              
+              echo "Select the option"
+              select option in "stop" "restart"
+              do
+                case $option in
+                   
+                  "stop")
+                    #Stopping the service
+                    systemctl stop $service_name
+                    clear
+                    echo "The $service_name service has been stopped"
+                    sleep 3
+                    clear
+                    break
+                    ;;
 
-              #Stopping the service
-              systemctl stop $service_name
+                  "restart")
+                    #Restarting the service
+                    systemctl restart $service_name
+                    clear
+                    echo "The $service_name has been restarted"
+                    sleep 3
+                    clear
+                    break
+                    ;;
+                  *)
+                    echo "The option does not exit"
+                esac
+              done
+            fi
+            
+            if [ $agree == "n" ];then
               clear
-              echo "The $service_name service has been stopped"
-          fi
-          
+              echo "Thank you Bye!"
+              sleep 3
+              clear
+            fi
+
      elif [ $check_service_status == "inactive" ];then
-          
+          clear
           echo "***************************************"
           echo "The $service_name service is not active"
           echo "***************************************"
-
+          sleep 3
+          clear
           read -p "Do you want to start the service[y/n[confirm]]:" confirm
-
+          
           if [ $confirm == "y" ];then
-              
+              clear
               echo "*************************************************************"
-              echo "The $service_name service has been started now....wait for 5s"
+              echo "The $service_name service has been started now....wait for 3s"
               echo "*************************************************************"
-              sleep 5
+              sleep 3
               clear
 
                #starting and checking the status for the service
                systemctl start $service_name
                systemctl status $service_name
-
-               sleep 5
+               sleep 3
                clear
 
-          else
-            
-            echo " ********************************************************"
-            echo " Thanks for checking the status of $service_name service"
-            echo " ********************************************************"
-            systemctl status $service_name
-            sleep 5 
-            clear
+               echo " ********************************************************"
+               echo " Thanks for checking the status of $service_name service"
+               echo " ********************************************************"
+               sleep 4 
+               clear
          fi
 
-       else
+         if [ $confirm == "n" ];then
+          clear
+          echo "Thank you Bye!"
+          sleep 3
+          clear
+         fi
+     else
 
-         echo "************************************************************"
-         echo " Please install the package if the service is not available."
-         echo "************************************************************"
+         echo "*************************************************************************************"
+         echo " Please install the package for $service_name service not available."
+         echo "**************************************************************************************"
+         sleep 10 
+         clear
+         
+         #This prompts the user to choose the linux distro running on the machine
+         #Creating an array
+       
+         declare -a distros
+         distros=("DEBIAN" "CENTOS" "FEDORA" "SUSE")
+         
+         echo "Select the linux distro running on your machine"
+
+         select distro in ${distros[@]}
+         do
+             sleep 0
+             clear
+             case $distro in
+               "DEBIAN")
+                 echo "Install the package using:"
+                 select tool in "FIREFOX" "TERMINAL"
+                 do
+                   case $tool in
+                     "FIREFOX")
+                       firefox
+                       clear
+                       break
+                       ;;
+                      "TERMINAL")
+                        echo "wait for the command to be chipped in"
+                        sleep 5
+                        clear
+                        break
+                        ;;
+                    esac
+                 done
+                break
+                ;;
+
+                "CENTOS")
+                    echo "Install the package using:"
+                    select tool in "FIREFOX" "TERMINAL"
+                    do
+                       case $tool in
+                          "FIREFOX")
+                            firefox
+                            clear
+                            break
+                            ;;
+                          "TERMINAL")
+                            echo "wait for the command to be chipped in"
+                            sleep 5
+                            clear
+                            break
+                            ;;
+                        esac
+                   done
+                   break
+                   ;;
+                   
+                   "FEDORA")
+                    echo "Install the package using:"
+                    select tool in "FIREFOX" "TERMINAL"
+                    do
+                       case $tool in
+                          "FIREFOX")
+                            firefox
+                            clear
+                            break
+                            ;;
+                          "TERMINAL")
+                            echo "wait for the command to be chipped in"
+                            sleep 5
+                            clear
+                            break
+                            ;;
+                        esac
+                   done
+                   break
+                   ;;
+                   
+                   "SUSE")
+                    echo "Install the package using:"
+                    select tool in "FIREFOX" "TERMINAL"
+                    do
+                       case $tool in
+                          "FIREFOX")
+                            firefox
+                            clear
+                            break
+                            ;;
+                          "TERMINAL")
+                            echo "wait for the command to be chipped in"
+                            sleep 5
+                            clear
+                            break
+                            ;;
+                        esac
+                   done
+                   break
+                   ;;
+               esac
+         done
    fi
 
 }
-
-#Invoki ng the function
-read -p "Enter the service name to check the status:" name
-service_control $name
+#Invoking the function
+read -p "Enter the service name to check the status:" srv_name
+service_control $srv_name
